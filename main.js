65e08ad3b4f50e82ffe709b2b28cf750e139d225
TejasviHari <73784362+TejasviHari@users.noreply.github.com>
2021-07-15 17:25:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+    canvas.parent('canvas');
+
+	instializeInSetup(mario);
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+    video.parent("game_console");
+
+
+	posenet=ml5.poseNet(video,modeloaded);
+	posenet.on("pose",gotResults);
+}
+
+function modeloaded(){
+	console.log("Model loaded!");
+}
+
+function gotResults(results){
+	if(results.length>0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+		console.log("noseX: "+noseX+" and noseY: "+noseY);
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
